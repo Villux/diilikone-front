2067a0d4c85fd6e1fa995ce33f7be87b2211da0d
Ville Toiviainen <villej.toiviainen@gmail.com>
2015-02-07 05:03:30 +0200

    Styled layout. Responsivenes, class year picker, magazin amount

--- src/app/main/userform.directive.js
+++ src/app/main/userform.directive.js
@@ -9,6 +9,21 @@ angular.module('diilikoneFront')
       templateUrl: 'app/main/userform.html',
       scope: {
         user: '=user'
+      },
+      link: function(scope, elem, attr) {
+        elem.find(".decrease-button").on('click', function() {
+          if (scope.user.deal_size === 25) {
+            return;
+          }
+          else {
+            scope.user.deal_size -= 25;
+          }
+          scope.$apply();
+        });
+        elem.find(".increase-button").on('click', function() {
+          scope.user.deal_size += 25;
+          scope.$apply();
+        });
       }
 
     };
